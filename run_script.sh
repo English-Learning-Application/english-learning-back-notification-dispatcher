002daa94ebe3341f80e236cb462d53b936c60651
Nguyen Phuong Tai <521h0480@student.tdtu.edu.vn>
2025-01-28 22:20:22 +0700

    .

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 761018889743.dkr.ecr.ap-southeast-2.amazonaws.com
-docker build -t course-microservice .
-docker tag course-microservice:latest 761018889743.dkr.ecr.ap-southeast-2.amazonaws.com/course-microservice:latest
-docker push 761018889743.dkr.ecr.ap-southeast-2.amazonaws.com/course-microservice:latest
-kubectl delete deployment course-service-deployment
+docker build -t notification-dispatcher-microservice .
+docker tag notification-dispatcher-microservice:latest 761018889743.dkr.ecr.ap-southeast-2.amazonaws.com/notification-dispatcher-microservice:latest
+docker push 761018889743.dkr.ecr.ap-southeast-2.amazonaws.com/notification-dispatcher-microservice:latest
+kubectl delete deployment dispatcher-service-deployment
 kubectl apply -f deployment.yaml
